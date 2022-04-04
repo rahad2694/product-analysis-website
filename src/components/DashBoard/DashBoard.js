010c6571b499026b228f3f6d268c0cbdf3fee6c3
rahad2694 <rahad.haw@gmail.com>
2022-04-05 03:17:30 +0600

    Area chart and bar chart added

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { CartesianGrid, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const DashBoard = () => {
     const [data, setData] = useState([]);
@@ -25,19 +25,65 @@ const DashBoard = () => {
             <div>
                 <p className='my-3 text-xl'>Group Wise Sales value</p>
                 <div className='flex justify-center'>
-                        <PieChart width={300} height={300}>
-                            <Pie
-                                dataKey="sell"
-                                isAnimationActive={false}
-                                data={data}
-                                cx="50%"
-                                cy="50%"
-                                outerRadius={100}
-                                fill="#8884d8"
-                                label
-                            />
-                            <Tooltip />
-                        </PieChart>
+                    <PieChart width={300} height={300}>
+                        <Pie
+                            dataKey="sell"
+                            isAnimationActive={false}
+                            data={data}
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={100}
+                            fill="#8884d8"
+                            label
+                        />
+                        <Tooltip />
+                    </PieChart>
+                </div>
+            </div>
+            <div>
+                <p className='my-3 text-xl'>Group Wise Revenue value</p>
+                <div className='flex justify-center'>
+                    <AreaChart
+                        width={400}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="revenue" stroke="#8884d8" fill="#8884d8" />
+                    </AreaChart>
+                </div>
+            </div>
+            <div>
+                <p className='my-3 text-xl'>Monthly Investment VS Revenue</p>
+                <div className='flex justify-center'>
+                    <BarChart
+                        width={400}
+                        height={280}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="investment" fill="#8884d8" />
+                        <Bar dataKey="revenue" fill="#82ca9d" />
+                    </BarChart>
                 </div>
             </div>
         </div>
