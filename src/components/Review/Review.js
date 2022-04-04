b4a78f70b83a0ab488712b9ddc62de0bf7c324ed
rahad2694 <rahad.haw@gmail.com>
2022-04-04 21:37:44 +0600

    fake db created for review

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Review = () => {
     return (
-        <div className=''>
-            regerrgeger
+        <div>
+            <div>
+                <h1 className='text-3xl font-bold my-5'>Our Best Customers (All)</h1>
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 };
+
 
 export default Review;
