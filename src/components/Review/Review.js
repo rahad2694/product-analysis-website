8be60637c0d1ddf0deaaba765304bbb5d578c8a7
rahad2694 <rahad.haw@gmail.com>
2022-04-05 01:25:41 +0600

    Review section finalized

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ShowReview from '../ShowReview/ShowReview';
 
 const Review = () => {
+    const [review , setReview] = useState([]);
+    useEffect(()=>{
+        fetch('reviewData.json')
+        .then(res => res.json())
+        .then(data => setReview(data));
+    },[]);
+    console.log(review);
     return (
         <div>
             <div>
                 <h1 className='text-3xl font-bold my-5'>Our Best Customers (All)</h1>
             </div>
-            <div>
-
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 mx-5'>
+                {
+                    review.map(item => <ShowReview key={item.id} item={item}></ShowReview>)
+                }
             </div>
         </div>
     );
---
+++ src/components/ShowReview/ShowReview.js
@@ -0,0 +1,20 @@
+import { faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './ShowReview.css';
+
+const ShowReview = (props) => {
+    const { feedback, image, name, rating } = props.item;
+    return (
+        <div className='single-review-container  shadow-lg shadow-gray-300 border p-5'>
+            <div className='flex justify-center'>
+                <img src={image} alt="" />
+            </div>
+            <h1 className='text-2xl font-bold'>{name}</h1>
+            <p>{rating} <span className='text-amber-400'><FontAwesomeIcon icon={faStar}></FontAwesomeIcon> {(rating%2===0 || rating ===3 || rating ===5)? <></> : <FontAwesomeIcon icon={faStarHalfStroke}></FontAwesomeIcon> }</span></p>
+            <p>{feedback}</p>
+        </div>
+    );
+};
+
+export default ShowReview;
